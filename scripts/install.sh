d713ec6128a6863812ed2819fc06f69d4a166843
Stephan Wendel <me@stephanwe.de>
2023-02-12 13:46:49 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -74,7 +74,7 @@ function welcome_msg() {
     printf "\n\033[31m#################### WARNING #####################\033[0m\n"
     printf "Make sure you are \033[31mnot\033[0m printing during install!\n"
     printf "All related services will be stopped!\n"
-    printf "\033[31m###################################################\033[0m\n\n"
+    printf "\033[31m##################################################\033[0m\n\n"
 }
 
 function dep_check_msg() {
