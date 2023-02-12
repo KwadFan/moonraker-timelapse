921523e64a6ca22ba24d51d763691be65f8e434f
Stephan Wendel <me@stephanwe.de>
2023-02-12 11:53:11 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ scripts/uninstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#### Moonraker Timelapse component uninstaller
+####
+#### Copyright (C) 2021 Christoph Frei <fryakatkop@gmail.com>
+#### Copyright (C) 2021 Stephan Wendel aka KwadFan <me@stephanwe.de>
+####
+#### This file may be distributed under the terms of the GNU GPLv3 license.
+####
+
+# shellcheck enable=require-variable-braces
+
+## Error handling
+set -Ee
+
+## Debug Option
+# set -x
