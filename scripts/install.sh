8e29a04166fab3e9c919c7f11fe0a1d1b251f118
Stephan Wendel <me@stephanwe.de>
2023-02-12 12:26:37 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,7 +31,7 @@ function ffmpeg_installed() {
 
 # Default Parameters
 MOONRAKER_TARGET_DIR="${HOME}/moonraker/moonraker/components"
-DATA_DIR=( "$(find "${HOME}" -maxdepth 1 -type d -name "*_data" -printf "%P ")" )
+DATA_DIR=( "$(find "${HOME}" -maxdepth 1 -type d -name "*_data" -printf "%P " | tr -d " $")" )
 FFMPEG_BIN="$(ffmpeg_installed)"
 DEPENDS_ON=( moonraker klipper )
 
