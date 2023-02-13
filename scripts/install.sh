dd0c6e18e5e615d82052edd72790f57dd380168e
Stephan Wendel <me@stephanwe.de>
2023-02-13 20:27:37 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -138,7 +138,6 @@ function get_instance_names() {
 function determine_data_structure() {
     ## See if there is more then 'klipper_config'
     get_instance_names
-    echo "${DATA_DIR[@]}"
     if [[ -d "${HOME}/klipper_config" ]] &&
     [[ "${#DATA_DIR[@]}" -eq 1 ]]; then
         DATA_DIR+=( klipper_config )
@@ -176,6 +175,7 @@ function link_component() {
 ### Link timelapse.cfg (Step 7)
 function link_macro_file() {
     local src path
+    echo "${#DATA_DIR[@]}"
     src="${SRC_DIR}/klipper_macro/timelapse.cfg"
     if [[ "${#DATA_DIR[@]}" -eq 1 ]] && [[ "${DATA_DIR[0]}" == "klipper_config" ]]; then
         link_to_msg "${DATA_DIR[0]}"
