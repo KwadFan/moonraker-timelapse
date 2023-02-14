be5229d7849718ace7308e3c330d5e3e885b0c16
Stephan Wendel <me@stephanwe.de>
2023-02-14 20:49:36 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -351,9 +351,11 @@ function config_hint_block(){
         return
     fi
     for i in "${DATA_DIR[@]}"; do
-        printf "\n\t- for Printer %s:\n" "${i/_data/}"
-        printf "\t[timelapse]\n\toutput_path: ~/%s/timelapse/\n" "${i}"
-        printf "\tframe_path: /tmp/timelapse/%s\n" "${i/_data/}"
+        if printf '%s\0' "${DATA_DIR[@]}" | grep -qxz -- "${i}"; then
+            printf "\n\t- for Printer %s:\n" "${i/_data/}"
+            printf "\t[timelapse]\n\toutput_path: ~/%s/timelapse/\n" "${i}"
+            printf "\tframe_path: /tmp/timelapse/%s\n" "${i/_data/}"
+        fi
     done
     return
 }
